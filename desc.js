d9777840a311908f2482325b06f6c4615d69cd03
Ororura <hitmanlol890@gmail.com>
2023-10-05 17:30:13 +0300

    bug fix

---
+++ desc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const desc = document.getElementsByClassName("desc");
+
+//desc.addEventListener("onclick", () => {
+//  desc[0].style.color = "red";
+//});
+
+//console.log(desc);
--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const sideBar = document.getElementsByClassName("side-bar")[0];
 const products = document.getElementsByClassName("products")[0];
 sideBar.style.transition = "150ms";
+let data;
 
 async function createProduct(i) {
   const res = await axios.get("https://fakestoreapi.com/products");
-  console.log(res.data.length)
+  console.log(res.data.length);
   for (i = 0; i < res.data.length; i++) {
     const productData = res.data[i];
 
@@ -14,15 +15,18 @@ async function createProduct(i) {
     newDiv.innerHTML = `
     <img src="${productData.image}" style="width: 150px;">
     <p>Id: ${productData.id}</p>
-    <p>Title: ${productData.title}</p>
-    <p>Description: ${productData.description} </p>
+    <p style="width: 223px; text-overflow: ellipsis; white-space: nowrap;
+    overflow: hidden;>Title: ${productData.title}</p>
+    <p class= "desc" style="width: 223px; text-overflow: ellipsis; white-space: nowrap;
+    overflow: hidden;">Description: ${productData.description} </p>
     <p>Price: ${productData.price} </p>
     <p>Category: ${productData.category} </p>
     <p>Rating: ${productData.rating.rate} </p>
     <p>Count: ${productData.rating.count} </p>
-
+    
   `;
     products.appendChild(newDiv);
+    data = res.data;
   }
 }
 
